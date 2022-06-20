1bb3c4ada0532885463b089951fac73e92ba778f
CrioDash <alex2727ko@gmail.com>
2022-06-20 14:34:58 +0300

    1

---
+++ Prog_Lab2_Task1/Task1.cs
@@ -0,0 +1,209 @@
+﻿namespace Prog_Lab2_Task1
+{
+    internal class Task1
+    {
+        public static void Main(string[] args)
+        {
+            SolarSystem solarSystem = new SolarSystem("Solar system");
+            Planet planet = new Planet("Earth",2342, Planet.PlanetType.SolidPlanet, solarSystem);
+            Satellite satellite = new Satellite("Moon", 123, Planet.PlanetType.SatellitePlanet, planet, solarSystem);
+            Star star = new Star("Sun", 34264563, Star.StarType.Default, solarSystem);
+            Console.WriteLine(solarSystem.ToString());
+            Console.WriteLine(planet.ToString());
+            Console.WriteLine(satellite.ToString());
+            Console.WriteLine(star.ToString());
+            Console.WriteLine($"В {solarSystem.Name} - {solarSystem.PlanetCount()} планет");
+            Console.Write($"В {solarSystem.Name} - {solarSystem.StarName()} зiрка");
+        }
+    }
+    
+    public class SolarSystem
+    {
+        public List<SpaceObject> Planets = new List<SpaceObject>();
+        
+        public Star Star { set; get; }
+        public string Name { set; get; }
+        public SolarSystem(string name)
+        {
+            Name = name;
+        }
+        public void AddPlanet(SpaceObject planet)
+        {
+            Planets.Add(planet);
+            Console.WriteLine($"{Name}: додано {planet.Name}");
+        }
+
+        public int PlanetCount()
+        {
+            return Planets.Count;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                return false;
+            SolarSystem m = obj as SolarSystem;
+            if (m as SolarSystem == null)
+                return false;
+
+            return m.Name == this.Name && m.Planets.Equals(this.Planets);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Planets, Name);
+        }
+
+        public string StarName()
+        {
+            return Star.Name;
+        }
+        
+        public override string ToString()
+        {
+            return "SolarSystem: " + Name + " " + Planets.Count;
+        }
+    }
+
+    public abstract class SpaceObject
+    {
+        public string Name { set; get; }
+        public int Mass { set; get; }
+        public SolarSystem System { set; get; }
+        public SpaceObject(string name, int mass, SolarSystem system)
+        {
+            Name = name;
+            Mass = mass;
+            System = system;
+        }
+        
+        public override string ToString()
+        {
+            return "SpaceObject: " + Name + " " + Mass + " " + System.Name;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                return false;
+            SpaceObject m = obj as SpaceObject;
+            if (m as SpaceObject == null)
+                return false;
+
+            return m.Name == this.Name && m.Mass == this.Mass && m.System.Equals(this.System);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Mass, System);
+        }
+    }
+
+    public class Star:SpaceObject
+    {
+        public enum StarType
+        {
+            Default,
+            BlackHole
+        }
+        public StarType Type { set; get; }
+        
+        public Star(string name, int mass, StarType type, SolarSystem system) : base(name, mass,system)
+        {
+            Type = type;
+            System = system;
+            system.Star = this;
+        }
+        
+        public override string ToString()
+        {
+            return "SpaceObject: " + Name + " " + Mass + " "+ Type + " "+ System.Name;
+        }
+        
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                return false;
+            Star m = obj as Star;
+            if (m as Star == null)
+                return false;
+
+            return base.Equals(m) && m.Type == this.Type && m.System.Equals(this.System);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), (int) Type);
+        }
+    }
+
+    public class Planet:SpaceObject
+    {
+        public enum PlanetType
+        {
+            SolidPlanet,
+            GasGiant,
+            SatellitePlanet,
+        }
+        public PlanetType Type { set; get; }
+        public Planet(string name, int mass, PlanetType type, SolarSystem system):base(name, mass,system)
+        {
+            Type = type;
+            System = system;
+            system.AddPlanet(this);
+        }
+        
+        public override string ToString()
+        {
+            return "SpaceObject: " + Name + " " + Mass + " "+ Type + " "+ System.Name;
+        }
+        
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                return false;
+            Planet m = obj as Planet;
+            if (m as Planet == null)
+                return false;
+
+            return base.Equals(m) && m.Type == this.Type;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), (int) Type);
+        }
+    }
+
+    public class Satellite : Planet
+    {
+        public Planet Parent { set; get; }
+        
+        public Satellite(string name, int mass, PlanetType type, Planet parent, SolarSystem system)
+            : base(name, mass, type, system)
+        {
+            Parent = parent;
+        }
+        
+        public override string ToString()
+        {
+            return "SpaceObject: " + Name + " " + Mass + " "+ Type + " " + Parent.Name + " " + System.Name;
+        }
+        
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                return false;
+            Satellite m = obj as Satellite;
+            if (m == null)
+                return false;
+
+            return base.Equals(m) && this.Parent.Equals(Parent);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), Parent);
+        }
+    }
+
+}
+
---
+++ Prog_Lab2_Task2/Task2.cs
@@ -0,0 +1,145 @@
+﻿namespace Prog_Lab2_Task2
+{
+    internal class Task2
+    {
+        public static void Main(string[] args)
+        {
+            Shop shop = new Shop("'Зiрочка'");
+            shop.AddProduct(new Product(Product.Type.Крупа,"Каша вiсяна 'Геркулес'",45,
+                new DateTime(2022,3,10),new DateTime(2022,9,10)));
+            shop.AddProduct(new Product(Product.Type.Солодощi,"'Шалена бджiлка'",100,
+                new DateTime(2020,1,15),new DateTime(2020,11,15)));
+            shop.AddProduct(new Product(Product.Type.Випiчка,"Хлiб Нiжинський",15,
+                new DateTime(2022,5,26),new DateTime(2022,5,27)));
+            shop.AddProduct(new Product(Product.Type.Випiчка,"Булка Здобна",20,
+                new DateTime(2022,5,24),new DateTime(2022,5,25)));
+            shop.SortProducts();
+            shop.FindProductDiapazone(new DateTime(2022, 5, 25),new DateTime(2022,5,27));
+            shop.FindEndedProducts();
+            shop.FindJanuaryProducts();
+        }
+    }
+
+    public class ProductComparer : IComparer<Product>
+    {
+        public int Compare(Product o1, Product o2)
+        {
+            if (o1.ProductType > o2.ProductType)
+                return 1;
+            if (o1.ProductType < o2.ProductType)
+                return -1; 
+            if (o1.Price > o2.Price)
+                return -1;
+            if (o1.Price < o2.Price)
+                return 1;
+            for (int i = 0; i < o1.Name.Length; i++)
+            {
+                if (o1.Name[i] > o2.Name[i] || i >= o2.Name.Length)
+                    return 1;
+                if (o1.Name[i] < o2.Name[i])
+                    return -1;
+            }
+            return 0;
+        }
+    }
+
+    public class Shop
+    {
+        public Shop(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { set; get; }
+        public ProductComparer pc = new ProductComparer(); 
+        public List<Product> ShopList = new List<Product>();
+
+        public void WriteShoplist(List<Product> list)
+        {
+            foreach (Product product in list)
+            {
+                Console.WriteLine(product.ProductType + " " + product.Name + " " + product.Price + "грн " + 
+                                  product.StartDate.Day+"."+product.StartDate.Month+"."+product.StartDate.Year + " " +
+                                  product.EndDate.Day+"."+product.EndDate.Month+"."+product.EndDate.Year);
+            }
+            Console.WriteLine();
+        }
+
+        public void FindProductDiapazone(DateTime d1, DateTime d2)
+        {
+            if (d2 < d1)
+                (d2, d1) = (d1, d2);
+            List<Product> list = new List<Product>();
+            foreach (Product product in ShopList)
+                if (product.EndDate >= d1 && product.EndDate <= d2)
+                    list.Add(product);
+            Console.WriteLine("Продукти, що мають бути вжитi з " +  d1.Day+"."+d1.Month+"."+d1.Year + " по " + 
+                              d2.Day+"."+d2.Month+"."+d2.Year + ":");
+            WriteShoplist(list);
+        }
+
+        public void FindEndedProducts()
+        {
+            List<Product> list = new List<Product>();
+            foreach (Product product in ShopList)
+                if(product.EndDate<DateTime.Today)
+                    list.Add(product);
+            Console.WriteLine("Просроченi продукти:");
+            WriteShoplist(list);
+        }
+
+        public void FindJanuaryProducts()
+        {
+            List<Product> list = new List<Product>();
+            foreach (Product product in ShopList)
+                if(product.StartDate.Year == 2020 && product.StartDate.Month == 1)
+                    list.Add(product);
+            Console.WriteLine("Продукти виготовленi у сiчнi 2020:");
+            WriteShoplist(list);
+        }
+
+        public void AddProduct(Product prod)
+        {
+            ShopList.Add(prod);
+        }
+
+        public void SortProducts()
+        {
+            ShopList.Sort(pc);
+            Console.WriteLine("Список продуктiв в магазинi " + Name + ":");
+            WriteShoplist(ShopList);
+        }
+    }
+
+    public class Product
+    {
+        public enum Type
+        {
+            Випiчка,
+            Крупа,
+            Мясо,
+            Овочi,
+            Риба,
+            Солодощi,
+            Снеки,
+            Фрукти
+        }
+
+        public Type ProductType { set; get; }
+        public string Name { set; get; }
+        public float Price { set; get; }
+        public DateTime StartDate { set; get; }
+        public DateTime EndDate { set; get; }
+
+        public Product(Type productType, string name, float price,DateTime startDate, DateTime endDate)
+        {
+            ProductType = productType;
+            Name = name;
+            Price = price;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+    }
+        
+}
